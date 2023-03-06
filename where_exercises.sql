3cdbf31f8c27bc0596905750c7f7ae2edee8bca1
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-03-06 14:32:42 -0600

    finished

--- where_exercises.sql
+++ where_exercises.sql
@@ -42,14 +42,14 @@ WHERE gender = ('M') AND (first_name = ('Irena') OR first_name = ('Vidya') OR fi
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE ('E%')
-AND last_name LIKE ('%E');
+OR last_name LIKE ('%E');
 
 
 
 # todo #4 Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
 SELECT first_name, last_name
 FROM employees
-WHERE last_name LIKE ('E%') AND (last_name LIKE '%E');
+WHERE last_name LIKE ('E%') AND last_name LIKE ('%E');
 
 
 
@@ -58,4 +58,4 @@ WHERE last_name LIKE ('E%') AND (last_name LIKE '%E');
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE ('%q%')
-  AND last_name NOT LIKE ('%qu%');
+AND last_name NOT LIKE ('%qu%');
