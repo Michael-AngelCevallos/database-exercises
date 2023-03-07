08bb29408dc08ee5fcc2ee04cdd457e08d93add2
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-03-07 15:09:46 -0600

    finished everything on join exercise except bonus

--- join_exercises.sql
+++ join_exercises.sql
@@ -34,15 +34,29 @@ WHERE dm.to_date = '9999-01-01' AND e.gender = 'F';
 
 
 # todo #4 Find the current titles of employees currently working in the Customer Service department.
-SELECT t.title AS Title , count(*) as Count /*t.titles grabs title of employee from employees table, count is used to count the number of employees */
+
+SELECT t.title AS Title, count(*) AS Number_Of_Employees
 FROM employees AS e
-JOIN dept_manager AS dm
-     ON dm.emp_no = e.emp_no
-JOIN departments AS d
-     ON dm.dept_no = d.dept_no
-Join titles AS t
-    ON e.emp_no = t.emp_no
-JOIN dept_emp AS de
-     ON de.emp_no = e.emp_no
-WHERE dm.to_date = '9999-01-01' AND d.dept_name = 'Customer Service Department' AND de.to_date = '9999-01-01'
+     JOIN dept_emp AS de
+              ON de.emp_no = e.emp_no
+     JOIN departments AS d
+              ON de.dept_no = d.dept_no
+     JOIN titles AS t
+              ON e.emp_no = t.emp_no
+WHERE t.to_date = '9999-01-01' AND d.dept_name = 'Customer Service' AND de.to_date = '9999-01-01'
 GROUP BY t.title;
+
+
+# todo #5 Find the current salary of all current managers. (once done , click on arrow to the right of Department name in terminal to re-arrange like example in codeup)
+
+SELECT d.dept_name AS Department_Name, concat(e.first_name, ' ', e.last_name) AS Manager_Name, sal.salary AS Salary
+FROM employees AS e
+Join salaries AS sal
+  ON  sal.emp_no = e.emp_no
+JOIN dept_manager AS dm
+    ON e.emp_no = dm.emp_no
+JOIN departments AS d
+    ON  d.dept_no = dm.dept_no
+WHERE dm.to_date = '9999-01-01' AND sal.to_date = '9999-01-01';
+
+# todo BONUS : Find the names of all current employees, their department name, and their current manager's name .
